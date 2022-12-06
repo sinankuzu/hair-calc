f308be1cf458dd7fb069e230e29aa0cd7c55d9fb
sinankuzu <sinankzu@gmail.com>
2022-12-06 13:18:22 +0100

    kk

--- hair-calc.js
+++ hair-calc.js
@@ -3,8 +3,11 @@ const pageOne = document.querySelector(".first-page")
 const pageTwo = document.querySelector(".second-page")
 
 const next = () => {
-    pageOne.style.left = "0";
-    pageTwo.style.left = ""
+    pageOne.style.transition = "all 5s";
+    pageOne.style.scale = "1.5"
+    pageOne.style.left = "-300px";
+    pageTwo.style.transition = "all 1s",
+    pageTwo.style.left = "-300px";
 }
 starten.addEventListener("click",next)
 

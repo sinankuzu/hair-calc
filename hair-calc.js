472f58656cb4a69155510364c2db644cd751dd6b
sinankuzu <sinankzu@gmail.com>
2022-12-06 18:09:11 +0100

    sa

--- hair-calc.js
+++ hair-calc.js
@@ -1,13 +1,33 @@
 const starten = document.querySelector(".button")
 const pageOne = document.querySelector(".first-page")
 const pageTwo = document.querySelector(".second-page")
+const pageThree = document.querySelector(".third-page")
+const man = document.querySelector(".man")
+const woman = document.querySelector(".woman")
+const level = document.querySelector(".level-1")
 
+
+let counter = 0;
 const next = () => {
-    pageOne.style.transition = "all 5s";
-    pageOne.style.scale = "1.5"
-    pageOne.style.left = "-300px";
-    pageTwo.style.transition = "all 1s",
-    pageTwo.style.left = "-300px";
-}
-starten.addEventListener("click",next)
-
+    if(counter == 0){
+        pageOne.classList.add("move-page")
+        pageTwo.classList.add("current-page")
+        level.style.width = "150px"
+        counter++
+        console.log(counter)
+        console.log(pageTwo.classList)
+    }
+    else if(counter == 1){
+        pageTwo.classList.add("move-page")
+        pageThree.classList.add("current-page")
+        level.style.width = "300px"
+        counter++
+    }
+    else if(counter == 2){
+        
+    }
+   
+};
+starten.addEventListener("click",next);
+man.addEventListener("click", next);
+woman.addEventListener("click", next);
